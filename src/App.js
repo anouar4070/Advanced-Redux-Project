7b60354bfe31d704aceb09b512900705c6468937
anouar garbaa <g.anouar@yahoo.com>
2024-10-23 16:39:57 +0100

    using UseEfeect() with redux to send a put request to Firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 function App() {
-const showCart = useSelector(state => state.ui.cartIsVisible );
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://advanced-redux-project-735fe-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
 
   return (
     <Layout>
